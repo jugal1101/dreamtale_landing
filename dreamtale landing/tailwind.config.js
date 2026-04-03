628ecd77c68ad6dbf9917e4b61e28ac4bac08d3a
jugal1101 <8733jugalrana@gmail.com>
2026-04-03 22:27:40 +0530

    Add files via upload

---
+++ dreamtale landing/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        baskervville: ['Baskervville', 'serif'],
+        display: ['Cinzel', 'serif'],
+        body: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
